5d4b0bb1f5900248bb1feb22cc17aa9c14c30f61
JiaJun <yimjiajun@icloud.com>
2022-12-01 21:48:18 +0800

    [upd] install.sh: move current vimrc into temporary directory as backup
    Signed-off-by: JiaJun <yimjiajun@icloud.com>

--- install.sh
+++ install.sh
@@ -1,3 +1,28 @@
 #! /bin/bash
 
+cat << EOF
+
+**********************************
+** VIM BY JIAJUN QUICK STARTUP  **
+**********************************
+
+EOF
+
+if [ -e ~/.vimrc ]
+then
+	echo "backup vim configuration file"
+	echo ">> .vimrc -> /tmp/.vimrc_backup"
+	mv ~/.vimrc /tmp/.vimrc_backup
+fi
+
 cp .vimrc ~/
+echo "updated vim configuration"
+echo ">> copied newest .vimrc"
+
+cat << EOF
+
+************************************
+** ENJOY VIM ! SUPPORT VIM SCRIPT **
+************************************
+
+EOF
